749465055639c87594ba7bd5ce209172613876f0
JSlander <jhovanslander@hotmail.com>
2023-03-21 21:27:30 -0500

    seeds partially added

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,20 @@
 const sequelize = require('../config/connection');
+const { User, Appointments } = require('../models');
 
+const userData = require('./userData.json');
+const appointmentsData = require('./projectData.json');
 
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
+  await Appointments.bulkCreate(appointmentsData, {
+    });
+  process.exit(0);
+};
 
+seedDatabase();
