113c89c2f3c44a063cb0b8f5f414bf3532725dbb
Connor S <constroh@gmail.com>
2023-03-23 19:02:28 -0500

    Service.js Model

---
+++ models/Service.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Service extends Model {}
+
+Service.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    service_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "service",
+  }
+);
+
+module.exports = Service;
