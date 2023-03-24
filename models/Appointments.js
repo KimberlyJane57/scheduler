1b44bbd4012634ca142a02e4a97199b0113b970e
KimberlyJane57 <kimberlythemotherofgod@gmail.com>
2023-03-23 18:43:40 -0600

    created appointment seeds

--- models/Appointments.js
+++ models/Appointments.js
@@ -2,13 +2,9 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class Appointments extends Model {}
 
-User.init(
+Appointments.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -55,18 +51,12 @@ User.init(
     },
   },
   {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: 'appointments',
   }
 );
 
-module.exports = User;
+module.exports = Appointments;
