8a7bdd44ee97a9088453e05ee2085024d74bac3f
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-23 19:58:22 -0500

    Model relationship

--- models/index.js
+++ models/index.js
@@ -1,1 +1,27 @@
-// This will be used to export models
+const User = require('./User');
+const Appointments = require('./Appointments');
+const Location = require('./Location');
+const Staff = require('./Staff')
+const Service = require('./Service');
+
+User.hasMany(Appointments, {
+  foreignKey: 'user_id',
+});
+
+Appointments.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Appointments.hasOne(Service, {
+    foreignKey: 'service_id',
+});
+
+Appointments.hasOne(Staff, {
+    foreignKey: 'staff_id',
+});
+
+Appointments.hasOne(Location, {
+    foreignKey: 'location_id',
+})
+
+module.exports = { User, Appointments, Location, Staff, Service };
