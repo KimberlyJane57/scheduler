839b5585ce02dc2c5b19cdfa95100e2da17dba4e
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-25 10:16:26 -0500

    Created logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,15 @@
-// Logout.js
+const logout = async (event) => {
+    event.preventDefault();
+    const response = await fetch('/api/user/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+
+ $('#logout').click(logout);
