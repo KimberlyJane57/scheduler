62504c27bcee31aa524009bd43c228a9f5f35c79
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-27 22:34:41 -0500

    Created dates, hours and dateTime seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,15 @@
 const sequelize = require('../config/connection');
-const { User, Appointments, Location, Service, Staff } = require('../models');
+const { User, Appointments, Location, Service, Staff, Dates, Hours, DateTime } = require('../models');
 
 const userData = require('./user.json');
 const locationData = require('./location.json');
 const serviceData = require('./service.json');
 const staffData = require('./staff.json');
 const appointmentsData = require('./appointments.json');
+const datesData = require('./dates.json');
+const hoursData = require('./hours.json');
+const dateTimeData = require('./dateTime.json');
+
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -19,6 +23,9 @@ const seedDatabase = async () => {
   await Service.bulkCreate(serviceData);
   await Staff.bulkCreate(staffData);
   await Appointments.bulkCreate(appointmentsData);
+  await Dates.bulkCreate(datesData);
+  await Hours.bulkCreate(hoursData);
+  await DateTime.bulkCreate(dateTimeData);
 
   process.exit(0);
 };
