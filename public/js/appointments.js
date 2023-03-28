11d4214efa6d38232c6e1f5336c56b468ed6dd73
JSlander <jhovanslander@hotmail.com>
2023-03-28 17:20:45 -0500

    dropdown 4 staff serv loc, delete still needs work

--- public/js/appointments.js
+++ public/js/appointments.js
@@ -3,11 +3,14 @@ const createAppt = async (event) => {
 
   const date = document.querySelector('#date').value.trim();
   const time = document.querySelector('#time').value.trim();
+  const staff = document.querySelector('#staff').value.trim();
+  const service = document.querySelector('#service').value.trim();
+  const location = document.querySelector('#location').value.trim();
 
-  if (date && time) {
+  if (date && time && staff && service && location) {
     const response = await fetch('/api/appointments/create', {
       method: 'POST',
-      body: JSON.stringify({ date, time }),
+      body: JSON.stringify({ date, time, staff, service, location}),
       headers: { 'Content-Type': 'application/json' },
     });
     if (response.ok) {
@@ -20,6 +23,9 @@ const createAppt = async (event) => {
 };
 const deleteAppt = async (event) => {
   event.preventDefault();
-};
+  const response = await fetch('/api/appointments/remove/<%=data[i].id%', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+})};
 $('#confirm-btn').click(createAppt);
 $('#delete-btn').click(deleteAppt);
